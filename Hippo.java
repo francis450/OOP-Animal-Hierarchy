7ecbf55ba01804cb633bc7f1563273ebc1ab0266
francis kamande <franciskamande2001@gmail.com>
2022-03-30 19:39:13 +0300

    Hippo is an Animal subclass and its concrete

---
+++ Hippo.java
@@ -0,0 +1,29 @@
+public class Hippo extends Animal{
+
+	//Hippos Local Methods
+	//A hippos Noise
+	public void makeNoise(){
+		System.out.println("I am a Hippooooooooo!!!!!!");
+	}
+
+	//Hippos food
+	public void eat(){
+		if(getHunger() >= 7){
+			int hun = getHunger();
+			hun--;
+			System.out.println("I guess it's eating time");
+		}else{
+			System.out.println("I am full!!");
+		}
+	}
+
+	//Where a Hippo sleeps
+	public void sleep(){
+		System.out.println("Ffffh! Ffffh!");
+	}
+
+	//How Does a Hippo roam
+	public  void roam(){
+		System.out.println("I am roaming at the "+getLocation());
+	}
+}
