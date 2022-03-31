ffcdd6142b14a545de44048a5ce1295cd419d88d
francis kamande <franciskamande2001@gmail.com>
2022-03-31 12:25:45 +0300

    Its a child to Feline and Animal parents

---
+++ Tiger.java
@@ -0,0 +1,19 @@
+public class Tiger extends Feline{
+
+	//Override abstract parent methods(Animal and Feline)
+	public void sleep(){
+		System.out.println("I sleep on a tree!");
+	}
+
+	public void eat(){
+		if(getHunger() > 5){
+			System.out.println("May I have some Antelope Please!");
+		}else{
+			System.out.println("Oh! Please I have had enough meat already!");
+		}
+	}
+
+	public void makeNoise(){
+		System.out.println("Roooaaaar!!!");
+	}
+}
