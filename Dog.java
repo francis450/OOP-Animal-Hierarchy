cc30360bdde862c4f6bbfeed755ad6e2d23da0ad
francis kamande <franciskamande2001@gmail.com>
2022-04-01 02:14:36 +0300

    Blueprint to a Dog object which is a child to the Canine and Animal

---
+++ Dog.java
@@ -0,0 +1,18 @@
+public class Dog extends Canine{
+	//Override abstract parent methods(Animal and canine)
+	public void sleep(){
+		System.out.println("I sleep on a House!");
+	}
+
+	public void eat(){
+		if(getHunger() > 5){
+			System.out.println("May I have some Dog Food Please!");
+		}else{
+			System.out.println("Oh! Please I have had enough Dog-foo already!");
+		}
+	}
+
+	public void makeNoise(){
+		System.out.println("Wooof!! Wooof!!");
+	}
+}
